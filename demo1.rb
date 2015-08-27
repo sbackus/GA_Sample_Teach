f9c1c47e575c87e338941bbca5a17d7247cf09ec
Sam Backus <Samuel.Backus@gmail.com>
2015-08-27 11:53:51 -0400

    add demos

---
+++ demo1.rb
@@ -0,0 +1,11 @@
+shopping_list = ["bread", "milk", "eggs"]
+shopping_list << "bananas"
+shopping_list << "kale"
+puts shopping_list
+
+
+todo = []
+todo << "shopping"
+todo << "laundry"
+todo << "yoga"
+puts todo
---
+++ demo2.rb
@@ -0,0 +1,8 @@
+us_presidents = ["Washington", "Adams", "Jefferson"]
+
+puts us_presidents.length
+puts us_presidents.empty?
+puts us_presidents.first
+puts us_presidents[0]
+puts us_presidents[1]
+
